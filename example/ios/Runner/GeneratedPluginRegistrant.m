a46d18c1c82647cf0975ee18c51a5693448ffcbd
sh <243280864@qq.com>
2020-05-06 14:16:16 +0800

    补充文件

---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,18 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<lsnetchatplugin/LsnetchatpluginPlugin.h>)
+#import <lsnetchatplugin/LsnetchatpluginPlugin.h>
+#else
+@import lsnetchatplugin;
+#endif
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [LsnetchatpluginPlugin registerWithRegistrar:[registry registrarForPlugin:@"LsnetchatpluginPlugin"]];
+}
+
+@end
