b6ecefc314a1bf0e696d7a1c0d708694a7c90ad0
sh <243280864@qq.com>
2020-05-06 14:23:51 +0800

    补充文件

---
+++ example/ios/Pods/Target Support Files/lsnetchatplugin/lsnetchatplugin-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_lsnetchatplugin : NSObject
+@end
+@implementation PodsDummy_lsnetchatplugin
+@end
---
+++ example/ios/Pods/Target Support Files/lsnetchatplugin/lsnetchatplugin-umbrella.h
@@ -0,0 +1,17 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "LsnetchatpluginPlugin.h"
+
+FOUNDATION_EXPORT double lsnetchatpluginVersionNumber;
+FOUNDATION_EXPORT const unsigned char lsnetchatpluginVersionString[];
+
