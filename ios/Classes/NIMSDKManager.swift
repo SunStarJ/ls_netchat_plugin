b28ba5e5425a0148d8007dc54bb921973b87892b
yunxiwangluo <liangming@52yunxi.cn>
2020-05-11 16:32:06 +0800

    又添加了聊天的一些方法

--- ios/Classes/NIMSDKManager.swift
+++ ios/Classes/NIMSDKManager.swift
@@ -12,28 +12,86 @@ class NIMSDKManager: NSObject{
     
     let appKey = "0267466fd2eca06140a495685764048d";
     
+    static let shareInstance = NIMSDKManager();
+    
     override init() {
         
         super.init();
         
+        initIM();
         
+    }
+    
+    func login(withAccount account: String,token: String){
+        //2.手动登录
+        NIMSDK.shared().loginManager.login(account, token: token) { [weak self] (error) in
+            self?.LMLogError(des: "手动登陆", error: error)
+        }
         
+        //自动登录
+        //NIMSDK.shared().loginManager.autoLogin("", token: "")
+    }
+    
+    
+    //初始化sdk
+    func initIM(){
         //sdk配置
         let regisOption = NIMSDKOption(appKey: appKey)
         //1.sdk初始化
         NIMSDK.shared().register(with: regisOption)
+    
+        NIMSDK.shared().chatroomManager.add(self)
         
-        //2.手动登录
-        NIMSDK.shared().loginManager.login("", token: "") { [weak self] (error) in
-         
-            self?.LMLogError(des: "手动登陆", error: error)
+    
+    }
+    
+    //加入聊天室
+    func joinChatRoom(withRoomId roomid: String){
+        
+        //添加聊天室监听
+        NIMSDK.shared().chatroomManager.add(self);
+        
+        
+        let request = NIMChatroomEnterRequest();
+        request.roomId = roomid;
+        
+        NIMSDK.shared().chatroomManager.enterChatroom(request) { (error, chatRoom, member) in
+            
+            print("进入聊天室：\(String(describing: error)),\(chatRoom),\(member)");
             
         }
         
-        //自动登录
-        NIMSDK.shared().loginManager.autoLogin("", token: "")
-        
     }
+    
+    //退出聊天室
+    func exitChatRoom(withRoomId roomid: String){
+        NIMSDK.shared().chatroomManager.exitChatroom(roomid) { (error) in
+            self.LMLogError(des: "退出聊天室", error: error);
+            
+            //这里退出聊天室释放监听
+            self.removeChatObsever()
+            
+        }
+    }
+    
+    func getChatRoomInfo(withRoomId roomid: String){
+        NIMSDK.shared().chatroomManager.fetchChatroomInfo(roomid) { (error, chatRoom) in
+            
+        }
+    }
+    
+    
+    
+    //添加消息监听
+    func addChatObsever(){
+        NIMSDK.shared().chatManager.add(self)
+    }
+    
+    //移出消息监听
+    func removeChatObsever(){
+        NIMSDK.shared().chatManager.remove(self)
+    }
+
     
     //发送一条文本消息
     func sendATextMessage(text: String,sessionId: String){
@@ -72,6 +130,7 @@ class NIMSDKManager: NSObject{
     
 }
 
+//登录监听回调
 extension NIMSDKManager: NIMLoginManagerDelegate{
     
     func onLogin(_ step: NIMLoginStep) {
@@ -82,4 +141,34 @@ extension NIMSDKManager: NIMLoginManagerDelegate{
         
     }
     
+}
+
+//聊天消息监听回调
+extension NIMSDKManager: NIMChatManagerDelegate{
+    
+    //收到消息回调
+    func onRecvMessages(_ messages: [NIMMessage]) {
+        
+    }
+    
+}
+
+//聊天室监听回调
+extension NIMSDKManager: NIMChatroomManagerDelegate{
+    
+    //被踢回调
+    func chatroomBeKicked(_ result: NIMChatroomBeKickedResult) {
+        
+    }
+    
+    //连接状态回调
+    func chatroom(_ roomId: String, connectionStateChanged state: NIMChatroomConnectionState) {
+        
+    }
+    
+    //自动登录出错回调
+    func chatroom(_ roomId: String, autoLoginFailed error: Error) {
+        
+    }
+
 }
--- ios/Classes/SwiftLsnetchatpluginPlugin.swift
+++ ios/Classes/SwiftLsnetchatpluginPlugin.swift
@@ -8,13 +8,48 @@ public class SwiftLsnetchatpluginPlugin: NSObject, FlutterPlugin {
     let instance = SwiftLsnetchatpluginPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
     
-    NIMSDKManager()
+    
     
     print("Bundle.main.bundleIdentifier is \(Bundle.main.bundleIdentifier)")
     
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+    
+    let dict = call.arguments as? [String:String]
+    
+    switch call.method {
+        case "initChatUtil":
+            NIMSDKManager.shareInstance.initIM();
+        break;
+        case "login":
+            NIMSDKManager.shareInstance.login(withAccount: dict?["account"] ?? "", token: dict?["token"] ?? "")
+        break;
+        case "enterChatRoom":
+            NIMSDKManager.shareInstance.joinChatRoom(withRoomId: dict?["roomId"] ?? "")
+        break;
+        case "exitChatRoom":
+            NIMSDKManager.shareInstance.exitChatRoom(withRoomId: dict?["roomId"] ?? "")
+        break;
+        case "sendTextMessage":
+            NIMSDKManager.shareInstance.sendATextMessage(text: dict?["message"] ?? "", sessionId: "")
+        break;
+        case "messageListener":
+            NIMSDKManager.shareInstance.addChatObsever()
+        break;
+        case "removeMessageListener":
+            NIMSDKManager.shareInstance.removeChatObsever()
+        break;
+        case "roomInfo":
+           // NIMSDKManager.shareInstance
+        break;
+
+    default:
+        break;
+    }
+    
+    
+    
     result("iOS " + UIDevice.current.systemVersion)
   }
 }
