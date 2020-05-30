8252ad08c7834bdf5fb6f5f527d3d215786fc1c6
sh <243280864@qq.com>
2020-05-30 16:33:17 +0800

    增加主线程验证

--- android/src/main/kotlin/com/example/lsnetchatplugin/NetChatApplication.kt
+++ android/src/main/kotlin/com/example/lsnetchatplugin/NetChatApplication.kt
@@ -4,12 +4,14 @@ import com.example.lsnetchatplugin.LsnetchatpluginPlugin.Companion.mContext
 import com.netease.nimlib.sdk.NIMClient
 import com.netease.nimlib.sdk.SDKOptions
 import com.netease.nimlib.sdk.msg.MsgService
+import com.netease.nimlib.sdk.util.NIMUtil
 import io.flutter.app.FlutterApplication
 
 abstract class NetChatApplication : FlutterApplication() {
     override fun onCreate() {
         super.onCreate()
         NIMClient.init(applicationContext, null, buildOptions(initApplicationKey()))
+        if(NIMUtil.isMainProcess(applicationContext))
         NIMClient.getService(MsgService::class.java).registerCustomAttachmentParser(CustomAttachParser())
     }
 
