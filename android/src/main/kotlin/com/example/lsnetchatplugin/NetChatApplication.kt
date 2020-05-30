6a7c68c1f095451618a63492f8ab1e0f802a9473
sh <243280864@qq.com>
2020-05-30 12:50:02 +0800

    增加导包

--- android/src/main/kotlin/com/example/lsnetchatplugin/NetChatApplication.kt
+++ android/src/main/kotlin/com/example/lsnetchatplugin/NetChatApplication.kt
@@ -3,6 +3,7 @@ package com.example.lsnetchatplugin
 import com.example.lsnetchatplugin.LsnetchatpluginPlugin.Companion.mContext
 import com.netease.nimlib.sdk.NIMClient
 import com.netease.nimlib.sdk.SDKOptions
+import com.netease.nimlib.sdk.msg.MsgService
 import io.flutter.app.FlutterApplication
 
 abstract class NetChatApplication : FlutterApplication() {
